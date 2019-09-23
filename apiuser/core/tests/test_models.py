d5c90c587e401ef56f56a614b0fcd56ff096dcf6
ngel rojas p <aomarrojasp@gmail.com>
2019-09-23 19:06:47 -0300

    add custom model

---
+++ apiuser/core/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """test creating a new user with an email is successful"""
+        email = 'me@ngelrojasp.com'
+        password = 'me123'
+        user = get_user_model().objects.create_user(
+                    email=email,
+                    password=password
+                )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_normalized(self):
+        """test the email for a new user is normalized"""
+        email = 'me@NGELROJASP.com'
+        user = get_user_model().objects.create_user(email, 'me123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'me123')
+
+    def test_create_new_superuser(self):
+        """test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+                'admin@ngelrojasp.com',
+                'admin123'
+                )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
