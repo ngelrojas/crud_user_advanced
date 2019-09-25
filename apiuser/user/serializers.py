eaa00ca826bb64c3f253b97e1e21860885f1bcbd
ngel rojas p <aomarrojasp@gmail.com>
2019-09-25 19:22:28 -0300

    function update user ended

--- apiuser/user/serializers.py
+++ apiuser/user/serializers.py
@@ -14,6 +14,17 @@ class UserSerializer(serializers.ModelSerializer):
         """create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validate_data)
 
+    def update(self, instance, validated_data):
+        """update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """serializer for the user authentication object"""
